39e44515a94bc0f1e624aa802c9ea6712758ac35
David Rivers <dmr41.rivers@gmail.com>
2015-02-19 11:40:56 -0700

    change adapter to work with new version of dotenv0.4

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import ENV from "../config/environment";
 
 export default DS.ActiveModelAdapter.extend({
-   host: ENV.adapterURL || ENV.ADAPTER_URL
+   host: ENV.adapterURL
 });
 
 // export default DS.ActiveModelAdapter.extend({
